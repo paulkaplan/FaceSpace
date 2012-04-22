a080a50537a5b9ba27b78e7c0301c5744dace23a
Sean Clemmer <sczizzo@gmail.com>
2012-04-22 15:26:28 -0500

    Whoops, forgot styles

--- public/javascripts/faceit.js
+++ public/javascripts/faceit.js
@@ -52,104 +52,104 @@ function execute(comp) {
   mesh.rotation.x   = yoff;
   camera.position.z = zoom * 100;
 
-  var setFreq = false;
+  var setFreq = true;
   var freq = -xoff * 1.42 * 1024 + 1024;
 
-  var C0 = 16.35;
-  var C1 = 32.70;
-  var C2 = 65.41;
-  var C3 = 130.81;
-  var C4 = 261.63;
-  var C5 = 523.25;
-  var C6 = 1046.50;
-
-  var D0 = 18.35;
-  var D1 = 36.71;
-  var D2 = 73.42;
-  var D3 = 146.83;
-  var D4 = 293.66;
-  var D5 = 587.33;
-  var D6 = 1174.66;
-
-  var G0 = 24.50;
-  var G1 = 49.00;
-  var G2 = 98.00;
-  var G3 = 196.00;
-  var G4 = 392.00;
-  var G5 = 783.99;
-  var G6 = 1567.98;
-
-  var A0 = 27.50;
-  var A1 = 55.00;
-  var A2 = 110.00;
-  var A3 = 220.00;
-  var A4 = 440.00;
-  var A5 = 880.00;
-  var A6 = 1760.00;
-
-  var E0 = 20.60;
-  var E1 = 41.20;
-  var E2 = 82.41;
-  var E3 = 164.81;
-  var E4 = 329.63;
-  var E5 = 659.26;
-  var E6 = 1318.51;
-
-  var F0 = 21.83;
-  var F1 = 43.65;
-  var F2 = 87.31;
-  var F3 = 174.61;
-  var F4 = 349.23;
-  var F5 = 698.46;
-  var F6 = 1396.91;
-
-  if (isNear(freq, C0)) { freq = C0; setFreq = true; }
-  if (isNear(freq, C1)) { freq = C1; setFreq = true; }
-  if (isNear(freq, C2)) { freq = C2; setFreq = true; }
-  if (isNear(freq, C3)) { freq = C3; setFreq = true; }
-  if (isNear(freq, C4)) { freq = C4; setFreq = true; }
-  if (isNear(freq, C5)) { freq = C5; setFreq = true; }
-  if (isNear(freq, C6)) { freq = C6; setFreq = true; }
-
-  if (isNear(freq, D0)) { freq = D0; setFreq = true; }
-  if (isNear(freq, D1)) { freq = D1; setFreq = true; }
-  if (isNear(freq, D2)) { freq = D2; setFreq = true; }
-  if (isNear(freq, D3)) { freq = D3; setFreq = true; }
-  if (isNear(freq, D4)) { freq = D4; setFreq = true; }
-  if (isNear(freq, D5)) { freq = D5; setFreq = true; }
-  if (isNear(freq, D6)) { freq = D6; setFreq = true; }
-
-  if (isNear(freq, G0)) { freq = G0; setFreq = true; }
-  if (isNear(freq, G1)) { freq = G1; setFreq = true; }
-  if (isNear(freq, G2)) { freq = G2; setFreq = true; }
-  if (isNear(freq, G3)) { freq = G3; setFreq = true; }
-  if (isNear(freq, G4)) { freq = G4; setFreq = true; }
-  if (isNear(freq, G5)) { freq = G5; setFreq = true; }
-  if (isNear(freq, G6)) { freq = G6; setFreq = true; }
-
-  if (isNear(freq, A0)) { freq = A0; setFreq = true; }
-  if (isNear(freq, A1)) { freq = A1; setFreq = true; }
-  if (isNear(freq, A2)) { freq = A2; setFreq = true; }
-  if (isNear(freq, A3)) { freq = A3; setFreq = true; }
-  if (isNear(freq, A4)) { freq = A4; setFreq = true; }
-  if (isNear(freq, A5)) { freq = A5; setFreq = true; }
-  if (isNear(freq, A6)) { freq = A6; setFreq = true; }
-
-  if (isNear(freq, E0)) { freq = E0; setFreq = true; }
-  if (isNear(freq, E1)) { freq = E1; setFreq = true; }
-  if (isNear(freq, E2)) { freq = E2; setFreq = true; }
-  if (isNear(freq, E3)) { freq = E3; setFreq = true; }
-  if (isNear(freq, E4)) { freq = E4; setFreq = true; }
-  if (isNear(freq, E5)) { freq = E5; setFreq = true; }
-  if (isNear(freq, E6)) { freq = E6; setFreq = true; }
-
-  if (isNear(freq, F0)) { freq = F0; setFreq = true; }
-  if (isNear(freq, F1)) { freq = F1; setFreq = true; }
-  if (isNear(freq, F2)) { freq = F2; setFreq = true; }
-  if (isNear(freq, F3)) { freq = F3; setFreq = true; }
-  if (isNear(freq, F4)) { freq = F4; setFreq = true; }
-  if (isNear(freq, F5)) { freq = F5; setFreq = true; }
-  if (isNear(freq, F6)) { freq = F6; setFreq = true; }
+  // var C0 = 16.35;
+  // var C1 = 32.70;
+  // var C2 = 65.41;
+  // var C3 = 130.81;
+  // var C4 = 261.63;
+  // var C5 = 523.25;
+  // var C6 = 1046.50;
+  // 
+  // var D0 = 18.35;
+  // var D1 = 36.71;
+  // var D2 = 73.42;
+  // var D3 = 146.83;
+  // var D4 = 293.66;
+  // var D5 = 587.33;
+  // var D6 = 1174.66;
+  // 
+  // var G0 = 24.50;
+  // var G1 = 49.00;
+  // var G2 = 98.00;
+  // var G3 = 196.00;
+  // var G4 = 392.00;
+  // var G5 = 783.99;
+  // var G6 = 1567.98;
+  // 
+  // var A0 = 27.50;
+  // var A1 = 55.00;
+  // var A2 = 110.00;
+  // var A3 = 220.00;
+  // var A4 = 440.00;
+  // var A5 = 880.00;
+  // var A6 = 1760.00;
+  // 
+  // var E0 = 20.60;
+  // var E1 = 41.20;
+  // var E2 = 82.41;
+  // var E3 = 164.81;
+  // var E4 = 329.63;
+  // var E5 = 659.26;
+  // var E6 = 1318.51;
+  // 
+  // var F0 = 21.83;
+  // var F1 = 43.65;
+  // var F2 = 87.31;
+  // var F3 = 174.61;
+  // var F4 = 349.23;
+  // var F5 = 698.46;
+  // var F6 = 1396.91;
+  // 
+  // if (isNear(freq, C0)) { freq = C0; setFreq = true; }
+  // if (isNear(freq, C1)) { freq = C1; setFreq = true; }
+  // if (isNear(freq, C2)) { freq = C2; setFreq = true; }
+  // if (isNear(freq, C3)) { freq = C3; setFreq = true; }
+  // if (isNear(freq, C4)) { freq = C4; setFreq = true; }
+  // if (isNear(freq, C5)) { freq = C5; setFreq = true; }
+  // if (isNear(freq, C6)) { freq = C6; setFreq = true; }
+  // 
+  // if (isNear(freq, D0)) { freq = D0; setFreq = true; }
+  // if (isNear(freq, D1)) { freq = D1; setFreq = true; }
+  // if (isNear(freq, D2)) { freq = D2; setFreq = true; }
+  // if (isNear(freq, D3)) { freq = D3; setFreq = true; }
+  // if (isNear(freq, D4)) { freq = D4; setFreq = true; }
+  // if (isNear(freq, D5)) { freq = D5; setFreq = true; }
+  // if (isNear(freq, D6)) { freq = D6; setFreq = true; }
+  // 
+  // if (isNear(freq, G0)) { freq = G0; setFreq = true; }
+  // if (isNear(freq, G1)) { freq = G1; setFreq = true; }
+  // if (isNear(freq, G2)) { freq = G2; setFreq = true; }
+  // if (isNear(freq, G3)) { freq = G3; setFreq = true; }
+  // if (isNear(freq, G4)) { freq = G4; setFreq = true; }
+  // if (isNear(freq, G5)) { freq = G5; setFreq = true; }
+  // if (isNear(freq, G6)) { freq = G6; setFreq = true; }
+  // 
+  // if (isNear(freq, A0)) { freq = A0; setFreq = true; }
+  // if (isNear(freq, A1)) { freq = A1; setFreq = true; }
+  // if (isNear(freq, A2)) { freq = A2; setFreq = true; }
+  // if (isNear(freq, A3)) { freq = A3; setFreq = true; }
+  // if (isNear(freq, A4)) { freq = A4; setFreq = true; }
+  // if (isNear(freq, A5)) { freq = A5; setFreq = true; }
+  // if (isNear(freq, A6)) { freq = A6; setFreq = true; }
+  // 
+  // if (isNear(freq, E0)) { freq = E0; setFreq = true; }
+  // if (isNear(freq, E1)) { freq = E1; setFreq = true; }
+  // if (isNear(freq, E2)) { freq = E2; setFreq = true; }
+  // if (isNear(freq, E3)) { freq = E3; setFreq = true; }
+  // if (isNear(freq, E4)) { freq = E4; setFreq = true; }
+  // if (isNear(freq, E5)) { freq = E5; setFreq = true; }
+  // if (isNear(freq, E6)) { freq = E6; setFreq = true; }
+  // 
+  // if (isNear(freq, F0)) { freq = F0; setFreq = true; }
+  // if (isNear(freq, F1)) { freq = F1; setFreq = true; }
+  // if (isNear(freq, F2)) { freq = F2; setFreq = true; }
+  // if (isNear(freq, F3)) { freq = F3; setFreq = true; }
+  // if (isNear(freq, F4)) { freq = F4; setFreq = true; }
+  // if (isNear(freq, F5)) { freq = F5; setFreq = true; }
+  // if (isNear(freq, F6)) { freq = F6; setFreq = true; }
 
   if (setFreq) { sinewave.setFrequency(freq); }
   sinewave.setAmplitude(yoff + 1);
@@ -191,4 +191,4 @@ function render() {
 
 var context  = new webkitAudioContext();
 var sinewave = new SineWave(context);
-sinewave.play();
+// sinewave.play();
